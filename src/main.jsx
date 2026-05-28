5441a6cb74224e25191f4ba0c3cd79a94e9282ff
fadka23 <fadhlyandriawan@gmail.com>
2026-05-28 15:40:19 +0700

    test : update

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")).render(
 			dayLabel="Day 4 of 4 TEESSTTTSSSSTSTSTS"
 			titleMain="Design Literacy"
 			titleAccent="Review & Storybook"
-			subtitle="A Storybook-ready version of your presentation design system."
+			subtitle="A Storybook-ready version aaaa of your presentation design system."
 			agenda={sampleAgenda}
 			duration="8 Hours"
 			sessions="3 + Mini-Project"
